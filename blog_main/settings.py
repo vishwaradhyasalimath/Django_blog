c2cf46446badc92f4532ad03c49495db007a86f1
vishwaradhya <vishwasalimath7@gmail.com>
2026-01-10 16:28:02 +0530

    Registeration and authentication

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blogs",
     "utility",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,12 @@
 
+from multiprocessing import AuthenticationError
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from blog_main.forms import RegistrationForm
 from blogs.models import Blog, Category
 from utility.models import About
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def home(request):
@@ -23,3 +27,43 @@ def home(request):
     }
 
     return render(request, 'home.html',context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+        form = RegistrationForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'register.html',context)
+
+
+def login(request):
+    #default authentication form 
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user =auth.aauthenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'login.html',context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
